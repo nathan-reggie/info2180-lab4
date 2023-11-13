84ccc4f50452e850d8dc33e4229e467ff4bcc1b1
Nathan Smith <nathan2012smith@gmail.com>
2023-11-12 22:14:53 -0500

    Commit app.js & html

--- app.js
+++ app.js
@@ -10,4 +10,24 @@ document.addEventListener("DOMContentLoaded", (event)=>{
         const data = await result.text()
         alert(data)
     })
+
+    const form = document.querySelector("#form")
+
+    form.addEventListener("submit", async (event)=>{
+        event.preventDefault()
+        const formData = new FormData(event.target)
+        const name = formData.get("name")
+        const result = await fetch(`http://localhost/info2180-lab4/superheroes.php?name=${name}`)
+        console.log(name)
+        const data = await result.json()
+        console.log(data, typeof(data))
+        const resultcont = document.querySelector(".result")
+        if(Object.keys(data).length > 1){
+            Object.keys(data).forEach(key =>{
+                let listElement = document.createElement("li")
+                listElement.innerHTML = data[key]["alias"]
+                resultcont.append(listElement)
+            })
+        }
+    })
 })
